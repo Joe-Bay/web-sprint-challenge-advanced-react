ae9b9d3613712016150ec7a7e07415e78dd55e07
Joe Bay <jbay55278@gmail.com>
2020-08-07 08:04:18 -0800

    Finished my testing and the sprint challenge is finished

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,41 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import App from '../App'
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<App />)
+    render(<CheckoutForm />)
+    const header = screen.getByText(/React Plants/i)
+    
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm />)
+
+    // find first name, lastname, address, city, state, zip code, and checkout button
+
+    const firstNameInput = screen.getByLabelText(/first name/i)
+    const lastNameInput = screen.getByLabelText(/last name/i)
+    const addressInput = screen.getByLabelText(/address/i)
+    const cityInput = screen.getByLabelText(/city/i)
+    const stateInput = screen.getByLabelText(/state/i)
+    const zipCodeInput = screen.getByLabelText(/zip/i)
+    const checkoutBtn = screen.getByRole('button', {name: /checkout/i})
+
+    // once all tests pass, time to fire events to type in different inputs
+
+    fireEvent.change(firstNameInput, {target: {value: 'Joe'}})
+    fireEvent.change(lastNameInput, {target: {value: 'Bay'}})
+    fireEvent.change(addressInput, {target: {value: '1234 N Lancealot Lane'}})
+    fireEvent.change(cityInput, {target: {value: 'Camelot'}})
+    fireEvent.change(stateInput, {target: {value: 'Alaska'}})
+    fireEvent.change(zipCodeInput, {target: {value: '54273'}})
+
+    fireEvent.click(checkoutBtn) // clicks the button
+
+    expect(screen.getByTestId(/successMessage/i)).toBeInTheDocument() // veryifys that the submitted result is showing in the document, checking by its Test ID
+
+});
